2659227ad910c59171fedf458975d8dad07e6239
Tiago Funk <tiagoff.tf@gmail.com>
2023-01-19 19:40:23 -0300

    Relationship between Customer and Address

--- clients/src/main/java/funk/tiago/clients/domain/Adress.java
+++ clients/src/main/java/funk/tiago/clients/domain/Adress.java
@@ -14,37 +14,52 @@ public class Adress {
     public String cep;
     public String number;
     public String city;
-    
+
+    public Adress(){}
+
     public Adress(String street, String cep, String number, String city) {
         this.street = street;
         this.cep = cep;
         this.number = number;
         this.city = city;
     }
+
     public String getStreet() {
         return street;
     }
+
     public void setStreet(String street) {
         this.street = street;
     }
+
     public String getCep() {
         return cep;
     }
+
     public void setCep(String cep) {
         this.cep = cep;
     }
+
     public String getNumber() {
         return number;
     }
+
     public void setNumber(String number) {
         this.number = number;
     }
+
     public String getCity() {
         return city;
     }
+
     public void setCity(String city) {
         this.city = city;
     }
 
-    
+    @Override
+    public String toString() {
+        return "Adress [id=" + id + ", street=" + street + ", cep=" + cep + ", number=" + number + ", city=" + city
+                + "]";
+    }
+
 }
--- clients/src/main/java/funk/tiago/clients/domain/Client.java
+++ clients/src/main/java/funk/tiago/clients/domain/Client.java
@@ -1,11 +1,15 @@
 package funk.tiago.clients.domain;
 
 import java.util.Date;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
 @Entity
 public class Client {
     @Id
@@ -13,26 +17,55 @@ public class Client {
     private int id;
     private String name;
     private Date birthdate;
-    
-    public Client() {}
-    public Client(String name, Date birthdate) {
+    @OneToMany(cascade=CascadeType.ALL)
+    private List<Adress> adresses;
+
+    public Client() {
+    }
+
+    public Client(String name, Date birthdate, List<Adress> adresses) {
         this.name = name;
         this.birthdate = birthdate;
+        this.adresses = adresses;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public Date getBirthdate() {
         return birthdate;
     }
+
     public void setBirthdate(Date birthdate) {
         this.birthdate = birthdate;
     }
+
     public int getId() {
         return id;
     }
-    
+
+    public List<Adress> getAdresses() {
+        return adresses;
+    }
+
+    public void setAdresses(List<Adress> adresses) {
+        this.adresses = adresses;
+    }
+
+    @Override
+    public String toString() {
+        String s = "Client [id=" + id + ", name=" + name + ", birthdate=" + birthdate + ", adresses=[\n";
+        for (Adress a : this.adresses) {
+            s += a.toString() + "\n";
+        }
+        s += "]";
+        return s;
+
+    }
+
 }
